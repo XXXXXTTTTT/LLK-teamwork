603f99f0eaf099d04f8f4c1ec13789a9193765fd
xdj66643 <142580490+xdj66643@users.noreply.github.com>
2024-12-24 19:14:56 +0800

    add: 倒计时三分钟

--- Bejeweled_Client/play.cpp
+++ Bejeweled_Client/play.cpp
@@ -4,12 +4,14 @@
 #include <QMenuBar>
 #include <QApplication>
 #include <QRandomGenerator>
+#include <QMessageBox>
 #include <QTimer>
 using namespace std;
 QGraphicsScene *scene_3;
 Play::Play(QWidget *parent)
     : QMainWindow(parent)
     , m_ui(new Ui::Play)
+    , remainingTime(180)
 {
     this->setWindowTitle("宝石迷阵");
     //this->setWindowIcon(QIcon(":/new/prefix1/ICON/13369429051CA2411D99F227A90D19CB9BE4EA10C2.jpg"));
@@ -75,15 +77,25 @@ Play::Play(QWidget *parent)
     //connect(m_ui->start, &QPushButton::clicked, m_board, &Board::generateBoard);
     connect(m_ui->pushButton, &QPushButton::clicked, m_board, &Board::updateBoard);
 
+    m_ui->lcdNumber->setDigitCount(5);
+    m_ui->lcdNumber->display("03:00");
+
+    m_timer = new QTimer(this);
+
+    // 连接定时器信号到更新槽函数
+    connect(m_timer, &QTimer::timeout, this, &Play::updateCountdown);
+
+    // 界面加载后自动开始倒计时
+    m_timer->start(1000);  // 每秒触发一次
 
     // 初始化计数器
-    count = 0;
+    //count = 0;
     // 创建定时器
-    timer = new QTimer(this);
+    //timer = new QTimer(this);
     // 连接定时器的timeout信号到updateLCD槽函数
-    connect(timer, &QTimer::timeout, this, &Play::updateziji);
+    //connect(timer, &QTimer::timeout, this, &Play::updateziji);
     // 设置定时器的更新时间间隔（比如 1000 毫秒，即每秒）
-    timer->start(1000);  // 每1秒触发一次timeout信号
+    //timer->start(1000);  // 每1秒触发一次timeout信号
 }
 
 Play::~Play()
@@ -102,8 +114,82 @@ void Play::updateButtonClicked() {
     m_board->updateBoard();
 }
 
+/*
 void Play::updateziji()
 {
     count++;  // 增加数字
     m_ui->ziji->display(count); // 更新显示的数字
 }
+*/
+
+void Play::updateCountdown() {
+    if (remainingTime > 0) {
+        --remainingTime;
+
+        // 格式化时间为 mm:ss
+        int minutes = remainingTime / 60;
+        int seconds = remainingTime % 60;
+        QString timeText = QString("%1:%2")
+                               .arg(minutes, 2, 10, QChar('0'))
+                               .arg(seconds, 2, 10, QChar('0'));
+        m_ui->lcdNumber->display(timeText);
+    } else {
+        m_timer->stop();
+        m_ui->lcdNumber->display("00:00");
+    }
+}
+
+void Play::checkGameOver(){
+    if (remainingTime <= 0) {
+        QMessageBox::information(this, "游戏结束", "时间到了！游戏失败！");
+        //resetGame();
+        return;
+    }
+
+    if (!hasValidMoves(m_board0)) {
+        QMessageBox::information(this, "游戏结束", "没有可匹配的宝石了！");
+        //resetGame();
+        return;
+    }
+}
+
+bool Play::hasValidMoves(const QVector<QVector<int>>& board){
+    int rows = board.size();
+    int cols = board[0].size();
+
+    // 遍历所有非空宝石
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            if (board[i][j] != 0) { // 非空宝石
+                for (int x = 0; x < rows; ++x) {
+                    for (int y = 0; y < cols; ++y) {
+                        if ((i != x || j != y) && board[i][j] == board[x][y]) {
+                            if (isPathValid(board, i, j, x, y)) {
+                                return true; // 存在合法匹配
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return false; // 没有合法匹配
+}
+
+bool Play::isPathValid(const QVector<QVector<int>>& board, int x1, int y1, int x2, int y2){
+    // 检查两颗宝石是否可以通过路径连接
+    // 此处可根据游戏规则具体实现，例如直线或两次拐角内可达
+    return true; // 示例中直接返回true
+}
+
+
+
+
+
+
+
+
+
+
+
+
--- Bejeweled_Client/play.h
+++ Bejeweled_Client/play.h
@@ -21,7 +21,7 @@ public:
     static QMainWindow* getUI();                 // 动画制作提供 UI
 
     int weizhiX(int);
-    int weizhiY(int);                                     // 析构函数
+    int weizhiY(int);                            // 析构函数
 
 signals:
     void statusCursorCchanger(int c);            // 信号
@@ -32,7 +32,10 @@ private slots:
                //void onResetButtonClicked();  // 手动重置按钮
     void startButtonClicked();
     void updateButtonClicked();
-    void updateziji();
+    //void updateziji();
+
+    void updateCountdown();  // 更新倒计时
+    void checkGameOver();    // 检查游戏结束
 
 private:
     Ui::Play *m_ui;                 // 指向 UI 类的指针，确保使用完整类型
@@ -40,11 +43,16 @@ private:
     QAction *m_hint;
     int m_score;
     bool m_begin = false;
-
+    QVector<QVector<int>> m_board0; // 游戏网格
     Board *m_board;
     int m_jewel[8][8] = {{0}};      // 匹配检测
 
-    QTimer *timer; // 定时器，显示在lcdnumber上
-    int count;      // 显示的数字
+    //QTimer *timer; // 定时器，显示在lcdnumber上
+    //int count;      // 显示的数字
+
+    QTimer *m_timer;       // 定时器
+    int remainingTime;   // 剩余时间（秒）
+    bool hasValidMoves(const QVector<QVector<int>>& board); // 是否有可匹配的宝石
+    bool isPathValid(const QVector<QVector<int>>& board, int x1, int y1, int x2, int y2);
 };
 #endif // PLAY_H
